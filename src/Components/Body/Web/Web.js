be0c2135ce62d946f6f5eb024bb3eeec16f5ffe8
Ofer Gilboa <ofer1gilboa@gmail.com>
2019-12-02 18:19:59 +0200

    priceless.3

--- src/Components/Body/Web/Web.js
+++ src/Components/Body/Web/Web.js
@@ -12,7 +12,8 @@ import CRM3 from '../../../img/CRM/Actions.PNG'
 import CRM4 from '../../../img/CRM/Analytics.PNG'
 import CRM5 from '../../../img/CRM/tryGif.gif'
 import PriceLess1 from '../../../img/PriceLess/homepage.png'
-import PriceLess2 from '../../../img/PriceLess/front.png'
+import PriceLess2 from '../../../img/PriceLess/concertPage.png'
+import PriceLess3 from '../../../img/PriceLess/front.png'
 import PodMan1 from '../../../img/PodMan/PodMan1.png'
 import PodMan2 from '../../../img/PodMan/PodMan2.PNG'
 import PodMan3 from '../../../img/PodMan/PodMan3.PNG'
@@ -42,7 +43,7 @@ const useStyles = makeStyles(theme => ({
 export default function SimpleExpansionPanel() {
    const classes = useStyles();
    let images1 = [CRM1, CRM2, CRM3, CRM4, CRM5]
-   let images2 = [PriceLess1, PriceLess2]
+   let images2 = [PriceLess1, PriceLess2, PriceLess3]
    let images3 = [PodMan1, PodMan2, PodMan3, PodMan4, PodMan5]
    let images4 = [OfersSite1, OfersSite2, OfersSite3, OfersSite4, OfersSite5, OfersSite6]
    const [index, setIndex] = React.useState(0);
