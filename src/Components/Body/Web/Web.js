d9c49bf6febf8be20bbfc7eb51ecee10855bfb3f
Ofer Gilboa <ofer1gilboa@gmail.com>
2020-03-30 14:18:11 +0300

    updating heroku

--- src/Components/Body/Web/Web.js
+++ src/Components/Body/Web/Web.js
@@ -6,11 +6,14 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import CRM1 from '../../../img/CRM/Clients.PNG'
-import CRM2 from '../../../img/CRM/ClientsByCountry.PNG'
-import CRM3 from '../../../img/CRM/Actions.PNG'
-import CRM4 from '../../../img/CRM/Analytics.PNG'
-import CRM5 from '../../../img/CRM/tryGif.gif'
+import CRM1 from '../../../img/CRM/Home.PNG'
+import CRM2 from '../../../img/CRM/Clients.PNG'
+import CRM3 from '../../../img/CRM/ClientsByCountry.PNG'
+import CRM4 from '../../../img/CRM/CRMclients.PNG'
+import CRM5 from '../../../img/CRM/Actions.PNG'
+import CRM6 from '../../../img/CRM/Analytics.PNG'
+import CRM7 from '../../../img/CRM/AnalyticsOwner.PNG'
+
 import PriceLess1 from '../../../img/PriceLess/homepage.png'
 import PriceLess2 from '../../../img/PriceLess/concertPage.png'
 import PriceLess3 from '../../../img/PriceLess/front.png'
@@ -42,7 +45,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function SimpleExpansionPanel() {
    const classes = useStyles();
-   let images1 = [CRM1, CRM2, CRM3, CRM4, CRM5]
+   let images1 = [CRM1, CRM2, CRM3, CRM4, CRM5, CRM6, CRM7]
    let images2 = [PriceLess1, PriceLess2, PriceLess3]
    let images3 = [PodMan1, PodMan2, PodMan3, PodMan4, PodMan5]
    let images4 = [OfersSite1, OfersSite2, OfersSite3, OfersSite4, OfersSite5, OfersSite6]
