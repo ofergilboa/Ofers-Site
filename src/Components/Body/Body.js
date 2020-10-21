5515af72f02734e87a02fa062dfe236a5e253511
Ofer Gilboa <ofer1gilboa@gmail.com>
2020-10-21 03:27:11 +0300

    with new version and no import

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -3,9 +3,9 @@ import './Body.css'
 import Projects from './Projects/Projects'
 import Arch from './Arch/Arch'
 import CV from './CV/CV'
-import AboutMe from './AboutMe/AboutMe'
-import SimpleExpansionPanel from './Projects/WebOld'
-import test1 from '../../img/Recorder/test1.mp4'
+// import AboutMe from './AboutMe/AboutMe'
+// import SimpleExpansionPanel from './Projects/WebOld'
+// import test1 from '../../img/Recorder/test1.mp4'
 
 
 class Body extends Component {
