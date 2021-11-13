f003b63ea462eb234b3721e8711149d8bf3fc856
Ofer Gilboa <ofer1gilboa@gmail.com>
2021-11-13 16:44:28 +0200

    Updates

---
+++ precache-manifest.b9946a1d001cdc4f47ec21adac35a085.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d542f91efe72f52c5adce0491b3b2a",
+    "url": "/Ofers-Site/index.html"
+  },
+  {
+    "revision": "c98aa71d8b3c5a068e7f",
+    "url": "/Ofers-Site/static/css/2.da817310.chunk.css"
+  },
+  {
+    "revision": "86c2c79a9b4629b8100c",
+    "url": "/Ofers-Site/static/css/main.1abd5483.chunk.css"
+  },
+  {
+    "revision": "c98aa71d8b3c5a068e7f",
+    "url": "/Ofers-Site/static/js/2.82d57bcc.chunk.js"
+  },
+  {
+    "revision": "86c2c79a9b4629b8100c",
+    "url": "/Ofers-Site/static/js/main.56f69029.chunk.js"
+  },
+  {
+    "revision": "267152049baf44eeefd3",
+    "url": "/Ofers-Site/static/js/runtime~main.404d90fa.js"
+  },
+  {
+    "revision": "f21bd9087ea98a58d93f8e32593b2b7d",
+    "url": "/Ofers-Site/static/media/Ofer.f21bd908.jpeg"
+  },
+  {
+    "revision": "b6e08b0a865d0d7518c438662251d26a",
+    "url": "/Ofers-Site/static/media/OferGilboaCV.b6e08b0a.pdf"
+  },
+  {
+    "revision": "20fc53303c3d2205b9d1d235432e49da",
+    "url": "/Ofers-Site/static/media/OferGilboaCVCut.20fc5330.jpeg"
+  },
+  {
+    "revision": "688a0d67c4b1b289c26575719aa2b826",
+    "url": "/Ofers-Site/static/media/OferGilboaCVslides.688a0d67.jpg"
+  },
+  {
+    "revision": "5516a117ae1472769c3fed2e7a12c751",
+    "url": "/Ofers-Site/static/media/video3.5516a117.mp4"
+  }
+]);
